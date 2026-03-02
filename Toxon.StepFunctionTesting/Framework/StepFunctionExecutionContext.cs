10e1abf2b35e0b1b120d6b9e417eb21958f9bc3c
Joel Shinu <joel.shinu@fundapps.co>
2026-03-02 17:58:46 +0000

    feat: add execution context

--- Toxon.StepFunctionTesting/Framework/StepFunctionExecutionContext.cs
+++ Toxon.StepFunctionTesting/Framework/StepFunctionExecutionContext.cs
@@ -7,7 +7,8 @@ internal record StepFunctionExecutionContext(
     JsonElement RootElement,
     IReadOnlyDictionary<string, IMockProvider> Mocks,
     ImmutableDictionary<string, int> Attempts,
-    ImmutableList<StepFunctionStateInvocation> Invocations
+    ImmutableList<StepFunctionStateInvocation> Invocations,
+    string ExecutionInput
 )
 {
     public string? QueryLanguage => RootElement.GetQueryLanguage();
